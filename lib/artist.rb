572139fbc97437e90c082f8e1fc3b9bcdef03005
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 17:07:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,18 +23,8 @@ class Artist
    self.all.detect { |artist| artist.name == name} || Artist.new(name)
  end 
  
- def self.find(name)
-   self.all.detect { |artist| artist.name == name}
- end
- 
- def self.create(name)
-   artist = Artist.new(name)
-   artist.save
-   artist
- end
- 
  def print_songs
-   self.songs.each {|song| song.name}
+   @songs.each { |song| puts song.name }
  end
  
 end 
