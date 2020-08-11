ac7e14585b30c0212674ac94b793aa44ea5c42c7
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 17:02:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,9 @@ class Song
   end
   
   def self.new_by_filename(file)
-    song_name = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
   end
   
   def artist_name=(name)
