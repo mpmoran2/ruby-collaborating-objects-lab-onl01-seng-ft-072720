22504e253102c761565e5c6a58d3941fa59e5c38
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 17:04:12 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,9 +9,7 @@ class MP3Importer
       @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
     end
 
-  def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
+ def import
+      self.files.each{|file| Song.new_by_filename(file)}
     end
-  end
 end
