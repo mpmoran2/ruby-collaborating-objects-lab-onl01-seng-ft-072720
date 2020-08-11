fb3192b12ba7394fc1d65dd099fd3b0e6c93b56c
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 17:37:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,4 @@
 class Song
-
   attr_accessor :name, :artist
 
   def initialize(name)
@@ -7,11 +6,16 @@ class Song
   end
 
   def self.new_by_filename(file)
-    split_name = filename.split(" - ")
-    song = self.new(split_name[1])
-    artist = Artist.find_or_create_by_name(split_name[0])
-    song.artist = artist
-    artist.add_song(song)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
     song
   end
-end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+end 
