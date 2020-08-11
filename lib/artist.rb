52808c42e3af3053bf4930df8cc8a84537dcf4e3
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 16:58:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,28 +19,25 @@ class Artist
     @all = []
   end 
   
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-  
-  def print_songs()
-    puts @songs.collect {|x| x.name}
-  end
+ def self.find_or_create_by_name(name)
+   if self.find(name)
+     self.find(name)
+   else
+     self.create(name)
+   end
+ end 
+ 
+ def self.find(name)
+   self.all.detect { |artist| artist.name == name}
+ end
+ 
+ def self.create(name)
+   artist = Artist.new(name)
+   artist.save
+   artist
+ end
+ 
+ def print_songs
+   self.songs.each {|song| song.name}
+ end
 end 
