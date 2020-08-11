f8486b9585375f9d3528511122817666b28b5e30
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 17:21:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
 class Artist 
+  
   attr_accessor :name, :songs 
+  
   @@all = []
   
   def initialize(name)
@@ -15,7 +17,7 @@ class Artist
     @@all << self
   end 
   
-  def all
+  def self.all
     @@all = []
   end 
   
