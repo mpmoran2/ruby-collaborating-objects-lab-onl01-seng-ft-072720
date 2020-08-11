2915ce45fa61742e7178d35ea3f649e27bc790cf
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 16:53:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,5 +19,24 @@ class Artist
     @all = []
   end 
   
-  
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
 end 
